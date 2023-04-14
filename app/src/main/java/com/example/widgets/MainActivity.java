af09b788feda9004399a1923294a05e18aca1a62
a21liltr <a21liltr@student.his.se>
2023-04-14 18:12:17 +0200

    Added new button, chained constraints to existing button with new button.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.widgets.databinding.ActivityMainBinding;
 
@@ -15,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String input = binding.editName.getText().toString();
+                binding.textView.setText("Hello " + input + "!");
+                binding.editName.setVisibility(View.GONE);
+                binding.button.setVisibility(View.GONE);
+            }
+        });
     }
 }
