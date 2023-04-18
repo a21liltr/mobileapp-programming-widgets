2123b81d3fd42554b9578b04f4c44c2d30443bd5
a21liltr <a21liltr@student.his.se>
2023-04-18 15:23:46 +0200

    Added descriptive messages for different events in MyListener class.

--- app/src/main/java/com/example/widgets/MyListener.java
+++ app/src/main/java/com/example/widgets/MyListener.java
@@ -22,22 +22,22 @@ public class MyListener implements View.OnClickListener{
         switch (view.getId()) {
             case R.id.buttonNo:
                 if (input.getText().toString().equals("")) {
-                    System.out.println("Blank");
+                    System.out.println("Already blank");
                     return;
                 }
                 binding.editName.getText().clear();
-                System.out.println("111");
+                System.out.println("Cleared EditText component");
                 break;
             case R.id.buttonYes:
                 if (input.getText().toString().equals("")) {
-                    System.out.println("Input is blank");
+                    System.out.println("Not allowed: Input is blank.");
                     return;
                 }
                 binding.textView.setText("Hello " + input.getText().toString() + "!");
                 binding.editName.setVisibility(View.GONE);
                 binding.buttonYes.setVisibility(View.GONE);
                 binding.buttonNo.setVisibility(View.GONE);
-                System.out.println("222");
+                System.out.println("Name is set.");
                 break;
             default:
                 System.out.println("Something went wrong!");
